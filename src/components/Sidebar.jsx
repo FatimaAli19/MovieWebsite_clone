c4e2fc5ad18972353481f3753a3e679d165e9ef1
FatimaAli19 <fatimaalimemon21@gmail.com>
2024-07-04 11:07:40 +0500

    Create the sidebar for footer

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import "../assets/styles.css";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import azlogo from "../assets/images/Screenshot 2024-07-03 121040.png";
+import { Link } from "react-router-dom";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-item">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="17"
+          height="20"
+          fill="#de1212"
+          class="bi bi-house-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+          <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+        </svg>{" "}
+        <Link to={"/"}>Home</Link>
+      </div>
+      <div className="sidebar-item">
+      <NavDropdown
+                  title={
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="#de1212"
+                        className="bi bi-film"
+                        viewBox="0 0 16 16"
+                        style={{ marginRight: "5px" }}
+                      >
+                        <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
+                      </svg>
+                      <span>Movies</span>
+                    </div>
+                  }
+                  id="basic-nav-dropdown"
+                  className="me-3 "
+                >
+                  <NavDropdown.Item href="https://desicinemas.tv/category/bollywood/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="#de1212"
+                      className="bi bi-film"
+                      viewBox="0 0 16 16"
+                      style={{ marginRight: "5px" }}
+                    >
+                      <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
+                    </svg>
+                    Bollywood
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="https://desicinemas.tv/category/hindi-dubbed/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="#de1212"
+                      class="bi bi-globe-asia-australia"
+                      viewBox="0 0 16 16"
+                      style={{ marginRight: "5px" }}
+                    >
+                      <path d="m10.495 6.92 1.278-.619a.483.483 0 0 0 .126-.782c-.252-.244-.682-.139-.932.107-.23.226-.513.373-.816.53l-.102.054c-.338.178-.264.626.1.736a.48.48 0 0 0 .346-.027ZM7.741 9.808V9.78a.413.413 0 1 1 .783.183l-.22.443a.6.6 0 0 1-.12.167l-.193.185a.36.36 0 1 1-.5-.516l.112-.108a.45.45 0 0 0 .138-.326M5.672 12.5l.482.233A.386.386 0 1 0 6.32 12h-.416a.7.7 0 0 1-.419-.139l-.277-.206a.302.302 0 1 0-.298.52z" />
+                      <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M1.612 10.867l.756-1.288a1 1 0 0 1 1.545-.225l1.074 1.005a.986.986 0 0 0 1.36-.011l.038-.037a.88.88 0 0 0 .26-.755c-.075-.548.37-1.033.92-1.099.728-.086 1.587-.324 1.728-.957.086-.386-.114-.83-.361-1.2-.207-.312 0-.8.374-.8.123 0 .24-.055.318-.15l.393-.474c.196-.237.491-.368.797-.403.554-.064 1.407-.277 1.583-.973.098-.391-.192-.634-.484-.88-.254-.212-.51-.426-.515-.741a7 7 0 0 1 3.425 7.692 1 1 0 0 0-.087-.063l-.316-.204a1 1 0 0 0-.977-.06l-.169.082a1 1 0 0 1-.741.051l-1.021-.329A1 1 0 0 0 11.205 9h-.165a1 1 0 0 0-.945.674l-.172.499a1 1 0 0 1-.404.514l-.802.518a1 1 0 0 0-.458.84v.455a1 1 0 0 0 1 1h.257a1 1 0 0 1 .542.16l.762.49a1 1 0 0 0 .283.126 7 7 0 0 1-9.49-3.409Z" />
+                    </svg>
+                    Hindi Dubbed
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="https://desicinemas.tv/category/hindi-dubbed/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="#de1212"
+                      className="bi bi-film"
+                      viewBox="0 0 16 16"
+                      style={{ marginRight: "5px" }}
+                    >
+                      <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
+                    </svg>
+                    Punjabi
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="https://desicinemas.tv/category/gujarati/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="#de1212"
+                      className="bi bi-film"
+                      viewBox="0 0 16 16"
+                      style={{ marginRight: "5px" }}
+                    >
+                      <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z" />
+                    </svg>
+                    Gujrati
+                  </NavDropdown.Item>
+                </NavDropdown>
+      </div>
+      <div className="sidebar-item">
+      <Link to={"/AZlist"}>
+                    {" "}
+                    <img
+                      src={azlogo}
+                      alt="azlogo"
+                      style={{ width: "30px", height: "30px" }}
+                    />
+                    A-Z List
+                  </Link>
+      </div>
+      <div className="sidebar-item">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="#de1212 "
+          class="bi bi-display"
+          viewBox="0 0 16 16"
+          style={{ marginRight: "5px" }}
+        >
+          <path d="M0 4s0-2 2-2h12s2 0 2 2v6s0 2-2 2h-4q0 1 .25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75Q6 13 6 12H2s-2 0-2-2zm1.398-.855a.76.76 0 0 0-.254.302A1.5 1.5 0 0 0 1 4.01V10c0 .325.078.502.145.602q.105.156.302.254a1.5 1.5 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.76.76 0 0 0 .254-.302 1.5 1.5 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.76.76 0 0 0-.302-.254A1.5 1.5 0 0 0 13.99 3H2c-.325 0-.502.078-.602.145" />
+        </svg>{" "}
+        INDIAN WEB SERIES
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
