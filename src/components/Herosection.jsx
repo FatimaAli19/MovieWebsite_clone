39f05f52decdd4e153943ef49b99d4301ce489c1
FatimaAli19 <fatimaalimemon21@gmail.com>
2024-07-05 11:07:01 +0500

    Create Herosection

---
+++ src/components/Herosection.jsx
@@ -0,0 +1,331 @@
+import Carousel from "react-bootstrap/Carousel";
+import poster1 from "../assets/images/poster.jpg";
+import poster2 from "../assets/images/poster1.jpg";
+import poster3 from "../assets/images/poster3.jpg";
+import poster4 from "../assets/images/poster4.jpg";
+import "../assets/styles.css";
+function Herosection() {
+   
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img src={poster1} alt="poster" className="d-block w-100 carousel-background" />
+        <Carousel.Caption className="carousel-bg">
+          <div>
+            <h3>
+              <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+                <div className="title">Bade Mian Chote Mian (HD)</div>
+              </a>
+            </h3>
+            <div className="info">
+              <div className="rating me-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13"
+                  height="13"
+                  fill="#de1212"
+                  class="bi bi-star-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+                <span> 0</span>
+              </div>
+              <span className="date me-2">2024</span>
+              <span className="movie me-2">movie</span>
+              <span className="time">2h 44m</span>
+            </div>
+            <p className="col-lg-6">
+              Elite soldiers team up to recover a stolen weapon from a masked
+              madman who wants to destroy India. Watch Online Links Link 01
+              TvLogy-S1 HD Click Here Link 02 TvLogy-S2 HD Click ...
+            </p>
+            <p>
+              <span>Director:</span>
+              <a href="https://desicinemas.privacystream.online/director/ali-abbas-zafar/">
+                Ali Abbas Zafar
+              </a>
+            </p>
+            <p>
+              <span>Genre:</span>
+              <a href="https://desicinemas.privacystream.online/category/action/">
+                Action
+              </a>
+              <a href="https://desicinemas.privacystream.online/category/bollywood/">
+                , Bollywood
+              </a>
+              <a href="https://desicinemas.privacystream.online/category/comedy/">
+                , Comedy
+              </a>
+              <a href="https://desicinemas.privacystream.online/category/science-fiction/">
+                , Science Fiction
+              </a>
+            </p>
+            <p>
+              <span>Cast:</span>
+              <a href="https://desicinemas.privacystream.online/cast/akshay-kumar/">
+                Akshay Kumar
+              </a>
+              <a href="https://desicinemas.privacystream.online/cast/akshay-kumar/">
+                Alaya F..
+              </a>
+            </p>
+            <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+              <button className="btn-red">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  fill="white"
+                  stroke="white"
+                  stroke-width="1.1"
+                  class="bi bi-play-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
+                </svg>
+                <strong> WATCH NOW</strong>
+              </button>
+            </a>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={poster2} alt="poster" className="d-block w-100 carousel-background" />
+        <Carousel.Caption className="carousel-bg">
+          <div>
+            <h3>
+              <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+                <div className="title">Dedh Bigha Zameen (HD)</div>{" "}
+              </a>
+            </h3>
+            <div className="info">
+              <div className="rating me-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13"
+                  height="13"
+                  fill="#de1212"
+                  class="bi bi-star-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+                <span> 0</span>
+              </div>
+              <span className="date me-2">2024</span>
+              <span className="movie me-2">movie</span>
+              <span className="time">2h 44m</span>
+            </div>
+            <p className="col-lg-6">
+              Anil, a 28-year-old wheat seller, lives with his wife,mother, and
+              sister. Excited for his sister Neha's marriage to Ajay, a
+              soon-to-be civil servant, Anil agrees to a dowry. To fund it, he
+              plans to sell his late ...
+            </p>
+            <p>
+              <span>Director:</span>
+              <a href="https://desicinemas.privacystream.online/director/pulkit/">
+                Pulkit
+              </a>
+            </p>
+            <p>
+              <span>Genre:</span>
+              <a href="https://desicinemas.privacystream.online/category/bollywood/">
+                Bollywood
+              </a>
+              <a href="https://desicinemas.tv/category/drama/">, Drama</a>
+              <a href="https://desicinemas.tv/category/family/">, family</a>
+            </p>
+            <p>
+              <span>Cast:</span>
+              <a href="https://desicinemas.tv/cast/durgesh-kumar/">
+                Durgesh Kumar
+              </a>
+              <a href="https://desicinemas.tv/cast/khushali-kumar/">
+                Khushali Kumar ...
+              </a>
+            </p>
+            <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+              <button className="btn-red">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  fill="white"
+                  stroke="white"
+                  stroke-width="1.1"
+                  class="bi bi-play-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
+                </svg>
+                <span> WATCH NOW</span>
+              </button>
+            </a>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={poster3} alt="poster" className="d-block w-100 carousel-background" />
+        <Carousel.Caption className="carousel-bg">
+          <div>
+            <h3>
+              <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+                <div className="title">
+                Ganapath (HD)
+                  </div> </a>
+            </h3>
+            <div className="info">
+              <div className="rating me-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13"
+                  height="13"
+                  fill="#de1212"
+                  class="bi bi-star-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+                <span> 0</span>
+              </div>
+              <span className="date me-2">2024</span>
+              <span className="movie me-2">movie</span>
+              <span className="time">2h 44m</span>
+            </div>
+            <p className="col-lg-6">
+              Anil, a 28-year-old wheat seller, lives with his wife,mother, and
+              sister. Excited for his sister Neha's marriage to Ajay, a
+              soon-to-be civil servant, Anil agrees to a dowry. To fund it, he
+              plans to sell his late ...
+            </p>
+            <p>
+              <span>Director:</span>
+              <a href="https://desicinemas.privacystream.online/director/pulkit/">
+                Pulkit
+              </a>
+            </p>
+            <p>
+              <span>Genre:</span>
+              <a href="https://desicinemas.privacystream.online/category/bollywood/">
+                Bollywood
+              </a>
+              <a href="https://desicinemas.tv/category/drama/">, Drama</a>
+              <a href="https://desicinemas.tv/category/family/">, family</a>
+            </p>
+            <p>
+              <span>Cast:</span>
+              <a href="https://desicinemas.tv/cast/durgesh-kumar/">
+                Durgesh Kumar
+              </a>
+              <a href="https://desicinemas.tv/cast/khushali-kumar/">
+                Khushali Kumar ...
+              </a>
+            </p>
+            <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+              <button className="btn-red">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  fill="white"
+                  stroke="white"
+                  stroke-width="1.1"
+                  class="bi bi-play-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
+                </svg>
+                <strong> WATCH NOW</strong>
+              </button>
+            </a>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={poster4} alt="poster" className="d-block w-100 carousel-background" />
+        <Carousel.Caption className="carousel-bg">
+          <div>
+            <h3>
+              <div className="title"></div>
+              <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+                <div className="title">Bhaiyya Ji (Low Quality)</div>{" "}
+              </a>
+            </h3>
+            <div className="info">
+              <div className="rating me-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13"
+                  height="13"
+                  fill="#de1212"
+                  class="bi bi-star-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+                <span> 0</span>
+              </div>
+              <span className="date me-2">2024</span>
+              <span className="movie me-2">movie</span>
+              <span className="time">2h 15m</span>
+            </div>
+            <p className="col-lg-6">
+              A retired outlaw embarks on a relentless mission of revenge
+              against the local mafia for the murder of his brother. Watch
+              Online Links Link 01 Flash Low Quality Click Here Link 02 TvLogy
+              Low...
+            </p>
+            <p>
+              <span>Director:</span>
+              <a href="https://desicinemas.privacystream.online/director/apoorv-singh-karki/">
+                Apoorv Singh Karki
+              </a>
+            </p>
+            <p>
+              <span>Genre:</span>
+              <a href="https://desicinemas.tv/category/action/">Action</a>
+              <a href="https://desicinemas.privacystream.online/category/bollywood/">
+                , Bollywood
+              </a>
+              <a href="https://desicinemas.tv/category/drama/">, Drama</a>
+            </p>
+            <p>
+              <span>Cast:</span>
+              <a href="https://desicinemas.privacystream.online/cast/jatin-goswami/">
+                Jatin Goswami,
+              </a>
+              <a href="https://desicinemas.privacystream.online/cast/manoj-bajpayee/">
+                Manoj Bajpayee ...
+              </a>
+            </p>
+            <a href="https://desicinemas.tv/movies/bade-miyan-chote-miyan-hd/">
+              <button className="btn-red">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  fill="white"
+                  stroke="white"
+                  stroke-width="1.1"
+                  class="bi bi-play-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
+                </svg>
+                <strong> WATCH NOW</strong>
+              </button>
+            </a>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Herosection;
