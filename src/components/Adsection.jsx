6470294eca09dda1d4d4a20c55973412b12dd942
FatimaAli19 <fatimaalimemon21@gmail.com>
2024-07-05 11:09:55 +0500

    Create section for TV shows

---
+++ src/components/Adsection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ad from '../assets/images/add.jpg'
+export default function Adsection() {
+  return (
+    <div className=' adsection '>
+        <a href="https://www.bollyzone.tv/series/"><img src={ad} alt="ad" /></a>
+    </div>
+  )
+}
